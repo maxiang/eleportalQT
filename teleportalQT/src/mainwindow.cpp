a6a6f9504d715681bb24c68d84d710c92ee2afb2
maxiang <maxkuaiyong@163.com>
2020-08-20 15:44:23 +0800

    1

--- teleportalQT/src/mainwindow.cpp
+++ teleportalQT/src/mainwindow.cpp
@@ -1002,15 +1002,10 @@ void MainWindow::RestartNetWork()
     manualControlTimer.stop();
     currentVehicle=1;
     firstRun = false;
+    armCheckBox->setChecked(false);
+    armCheckBox_stateChanged(Qt::Unchecked);
     AS::as_api_deinit();
     std::string ip("192.168.2.");
-    //disarm
-    bool bCheck=armCheckBox->isChecked();
-    if(bCheck)
-    {
-        armCheckBox->setChecked(false);
-        armCheckBox_stateChanged(Qt::Unchecked);
-    }
 
     AS::as_api_init(ip.c_str(), F_THREAD_ALL);
 
@@ -1019,12 +1014,7 @@ void MainWindow::RestartNetWork()
     rollLPitchCheckTimer.start();
     vehicleDataUpdateTimer.start();
     manualControlTimer.start();
-    //reset arm
-    if(bCheck)
-    {
-        armCheckBox->setChecked(true);
-        armCheckBox_stateChanged(Qt::Checked);
-    }
+
 }
 
 void MainWindow::LoadInIConfig()
